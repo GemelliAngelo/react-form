f02e7f50912660345136196f4db655274bd21674
GemelliAngelo <gemeangelo27@gmail.com>
2024-12-03 14:46:53 +0100

    add post handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import { useState } from "react";
 
 function App() {
-  const [titles, setTitles] = useState();
+  const [addTitles, setAddTitles] = useState("");
+  const [posts, setPosts] = useState([]);
 
-  const handleInputTitles = (e) => {
-    console.log(e.target.value);
+  const handleInputChange = (e) => {
+    setAddTitles(e.target.value);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    setPosts([...posts, { name: addTitles }]);
+    setAddTitles("");
+    console.log(addTitles);
   };
 
   return (
@@ -15,11 +23,25 @@ function App() {
       <main>
         <div className="container">
           <div className="d-flex">
-            <form onChange={handleInputTitles}>
-              <input type="text" className="form-input" />
+            <form onSubmit={handleFormSubmit}>
+              <input
+                onChange={handleInputChange}
+                type="text"
+                className="form-input"
+                name="blog-title"
+                value={addTitles}
+              />
               <button className="form-button">AGGIUNGI</button>
             </form>
-            <ul></ul>
+            <div className="d-flex">
+              <ul className="titles-list">
+                {posts.map((post) => (
+                  <li className="titles-list-item" key={post.name}>
+                    {post.name}
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
       </main>
