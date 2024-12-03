9d447c9a31af9ab4b8ecc888bd0951e524257007
GemelliAngelo <gemeangelo27@gmail.com>
2024-12-03 13:34:15 +0100

    add state + handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import { useState } from "react";
 
 function App() {
+  const [titles, setTitles] = useState();
+
+  const handleInputTitles = (e) => {
+    console.log(e.target.value);
+  };
+
   return (
     <>
       <header>
@@ -9,10 +15,11 @@ function App() {
       <main>
         <div className="container">
           <div className="d-flex">
-            <form>
+            <form onChange={handleInputTitles}>
               <input type="text" className="form-input" />
               <button className="form-button">AGGIUNGI</button>
             </form>
+            <ul></ul>
           </div>
         </div>
       </main>
