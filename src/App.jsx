bd8c4cb11b54b8d590de116264a600c123828064
GemelliAngelo <gemeangelo27@gmail.com>
2024-12-03 13:24:53 +0100

    add main + form + style

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,16 @@ function App() {
       <header>
         <h1>REACT BLOG FORM</h1>
       </header>
+      <main>
+        <div className="container">
+          <div className="d-flex">
+            <form>
+              <input type="text" className="form-input" />
+              <button className="form-button">AGGIUNGI</button>
+            </form>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
